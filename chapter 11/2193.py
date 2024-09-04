a2f40e4350dee3aca75aecee124baceb03fd1189
잉퓨 <dayeon620@kakao.com>
2024-09-04 18:15:24 +0900

    chapter 11

---
+++ chapter 11/2193.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+D = [[0 for _ in range(2)]for _ in range(n+1)]
+
+D[1][0] = 0
+D[1][1] = 1
+
+for i in range(2, n+1):
+    D[i][0] = D[i-1][1] + D[i-1][0]
+    D[i][1] = D[i-1][0]
+
+print(D[n][0] + D[n][1])
